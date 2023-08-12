a3a0871749b23ae54eee6014d332ac2126894199
Shoaib Dar <SDAR30@gmail.com>
2023-08-12 18:59:12 -0400

    initialize directory structure

---
+++ src/app/login/page.tsx
@@ -0,0 +1,8 @@
+export default function loginPage() {
+
+    return (
+        <div className="flex ">
+            <h1 className="text-center text-white text-2xl">Login</h1>
+        </div>
+    )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,8 @@
+export default function SignupPage() {
+
+    return (
+        <div className="flex ">
+            <h1 className="text-center text-white text-2xl">SIGNN UP</h1>
+        </div>
+    )
+}
