e379136ce3a6b087351782e190061ab2087d454b
Shoaib Dar <SDAR30@gmail.com>
2023-08-13 16:07:35 -0400

    signup client-side request initalized

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,16 +2,30 @@
 import Link from "next/link";
 import React, {useState, useEffect} from "react";
 import { useRouter } from 'next/navigation';
-import { axios } from "axios";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 
 
 export default function SignupPage() {
     const router = useRouter();
     const [user, setUser] = useState({ email: "", password: "", username: "" })
-    const [buttonDisabled, setButtonDisabled] = React.useState(true)
+    const [buttonDisabled, setButtonDisabled] = useState(true)
+    const [loading, setLoading] = useState(false);
 
     const onSignup = async () => {
+        try {
+            setLoading(true);
+            const response = await axios.post("/api/users/signup", user);
+            console.log('signup success', response.data)
+            router.push('/login');
+            
+        } catch (error: any) {
+            console.log('error in onSignup catcH: ',error )
+            toast.error(error.message);
+        }finally{
+            setLoading(false)
+        }
 
     }
 
@@ -26,7 +40,7 @@ export default function SignupPage() {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1 className="text-center text-white text-2xl">SIGNN UP</h1>
+            <h1 className="text-center text-white text-2xl">{loading ? "processing": "sign UP" }</h1>
             <hr />
             <label htmlFor="username">username</label>
             <input
