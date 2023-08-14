464e7d6bbb6416d6afd4f47eb7da88493e815892
Shoaib Dar <SDAR30@gmail.com>
2023-08-14 12:46:24 -0400

    implemented logout button on profile page

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        const response = NextResponse.json({message: "loggg out succesfful", success: true})
+        response.cookies.set("token", "", {httpOnly: true, expires: new Date(0)})
+        return response;
+        
+    } catch (error: any) {
+        console.log("caught error in logout/route.ts", error)
+        return NextResponse.json({ erorr: error.message }, { status: 500 })
+
+    }
+}
