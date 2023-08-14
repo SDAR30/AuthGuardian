ee733a4bc21743bee48d7efec40ab113c0b3b4e9
Shoaib Dar <SDAR30@gmail.com>
2023-08-14 14:05:03 -0400

    setup middlerware for default login page

---
+++ src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+//   return NextResponse.redirect(new URL('/home', request.url))
+    const path = request.nextUrl.pathname;
+    const isPublicPaths = path === '/login' || path === '/signup';
+    //either token exists, or get '';
+    const token = request.cookies.get('token')?.value || '';
+    if(isPublicPaths && token){
+        return NextResponse.redirect(new URL('/', request.nextUrl));
+    }
+
+    if(!isPublicPaths && !token){
+        return NextResponse.redirect(new URL('/login', request.nextUrl))
+    }
+
+
+}
+ 
+// See "Matching Paths" below to learn more. match to run middleware
+export const config = {
+  matcher: ['/', '/profile','/login','/signup',],
+}
