3cfda266927503a4badb40d4469ee52fa41472e2
Shoaib Dar <SDAR30@gmail.com>
2023-08-16 19:30:49 -0400

    error log handling for getDataFromToken token

--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -1,15 +1,17 @@
 import { NextRequest } from "next/server";
-import  jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
 export const getDataFromToken = (request: NextRequest) => {
     try {
         const token = request.cookies.get('token')?.value || '';
+        if (!token) throw new Error("TOKEN NoT PROVIDED");
+        console.log('from getDataFromToken: token: ', token)
         const decodedToken: any = jwt.verify(token, process.env.TOKEN_SECRET!);
         console.log('decoding token: ', decodedToken)
 
         return decodedToken.id;
-        
+
     } catch (error: any) {
-            throw new Error(error.message)
+        throw new Error(error.message)
     }
 }
