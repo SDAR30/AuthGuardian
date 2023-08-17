8833e0c486fae204f5b9a87aacf47c4c4e8650f0
Shoaib Dar <SDAR30@gmail.com>
2023-08-17 12:13:06 -0400

    new implementation profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -7,19 +7,13 @@ import { useState } from "react";
 
 export default function ProfilePage() {
     const router = useRouter();
-
-    type UserData = {
-        _id?: string;
-        username?: string;
-        email?: string;
-        isVerified?: boolean;
-        isAdmin?: boolean;
-    };
-
-    const [data, setData] = useState<UserData>({});
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [verified, setVerified] = useState(false)
 
     const logout = async () => {
         try {
+            setLoading(true);
             await axios.get("/api/users/logout");
             toast.success("sucesfuly logged out")
             router.push('/login')
@@ -27,16 +21,77 @@ export default function ProfilePage() {
         } catch (error: any) {
             console.log("caught error in profile/logout/route", error)
             toast.error(error.message)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const fetchUser = async () => {
+        try {
+            const response = await axios.get('/api/users/me');
+            return response
+
+        } catch (error) {
+            console.log("error in fetchUSER")
+        }
+    }
+
+    const userProfile = async () => {
+        try {
+
+            const response: any = await fetchUser();
+
+            if (response.data.success) {
+                toast.success("Redirecting....")
+            }
+            const userId = response.data.user._id;
+
+            router.push(`/profile/${userId}`)
+
+        } catch (error: any) {
+            console.log("error in userProfile", error)
+        }
+    }
+
+    const getUser = async () => {
+        try {
+            const response: any = await fetchUser();
+            const userId = response.data.user._id;
+
+            setData(userId)
+
+        } catch (error: any) {
+            console.log("error in getUSER", error)
+        }
+    }
+
+    const handleVerify = async () => {
+        try {
+            const response: any = await fetchUser();
+            setVerified(response.data.user.isVerified);
+            const { email, _id } = await response.data.user;
+            const emailType = "VERIFY";
+            const userID = _id;
+            toast.loading('verifying email...')
+            const emailSender = await axios.post('/api/users/sendEmail', { email, emailType, userID })
+            console.log("handleVerify: email sender: ", emailSender)
+            toast.dismiss();
+            toast.success('EMAIL VERIIFEDD SUCESFULLY');
+            setVerified(true);
+
+        } catch (error: any) {
+            console.log("error in handleVerify: ", error)
         }
 
     }
 
-    const getUserDetails = async () => {
-        const res = await axios.get('/api/users/me');
-        console.log("getUserDetails ", res.data)
-        setData(res.data.data)
 
-    }
+    // const getUserDetails = async () => {
+    //     const res = await axios.get('/api/users/me');
+    //     console.log("getUserDetails ", res.data)
+    //     setData(res.data.data)
+
+    // }
 
     // useEffect(() => {
     //     const fetchUserDetails = async () => {
@@ -59,26 +114,27 @@ export default function ProfilePage() {
                 <Link href="/">Home Page</Link>
             </div>
             <div className="flex flex-col items-center justify-center min-h-screen py-2">
-                <h1 className="text-5xl m-3">{data.username}</h1>
-                {/* <hr />
-                <p>Email: {data.email} </p>
-                <p>ID: {data._id}</p> */}
+                <p className='text-sm ml-[5.55rem] mt-2'>{
+                    verified ? (
+                        <p className='text-green-500'>Verified</p>
+                    ) : (
+                        <button onClick={handleVerify}>Click here to Verify</button>
+                    )
+                }</p>
                 <hr />
-                <hr />
-                <h2 className="bg-purple-900 mt-4 hover:bg-purple-700 text-white py-2 px-4 rounded">
-                    {!data.username ? "-----" : <Link href={`/profile/${data.username}`}>Go to default profile page</Link>}
-                </h2>
+                <div className='mt-10 flex flex-col text-white justify-center items-center'>
+                    <p className='text'>User Details: {data ? `${data}` : "null"}</p>
+                    <button disabled={data ? true : false} className=' disabled:opacity-50 text-sm w-[150px] mt-3 bg-orange-500 text-black rounded-full px-4 py-2' onClick={getUser}>Get User Details</button>
+                </div>
+
+                <div className='mt-5'>
+                    <button onClick={userProfile} className='text-sm mt-3 bg-orange-500 text-black rounded-full px-2 py-2'>Go to my profile</button>
+                </div>
 
                 <h2 className="bg-yellow-500 mt-4 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded">
                     <Link href="/resetpassword">Change password</Link></h2>
 
-                <button className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                    onClick={logout}
-                >Log out</button>
-
-                <button className="bg-green-300 mt-4 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-                    onClick={getUserDetails}>  get username
-                </button>
+                <button className='text-xl mt-3 bg-orange-500 text-black rounded-full px-4 py-2' onClick={logout}>{loading ? "loading..." : "Logout"}</button>
 
             </div>
         </>
--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -3,11 +3,8 @@ import jwt from "jsonwebtoken";
 
 export const getDataFromToken = (request: NextRequest) => {
     try {
-        // console.log('All cookies: ', request.cookies);
         const token = request.cookies.get('token')?.value || '';
-        console.log('from getDataFromToken: token: ', token)
         const decodedToken: any = jwt.verify(token, process.env.TOKEN_SECRET!);
-        console.log('decoding token: ', decodedToken)
 
         return decodedToken.id;
 
